ab608eb1b4c37ac585c213874911d9016d481f4d
Pratik Kamble <prak@ciklum.com>
2025-04-15 15:36:35 +0530

    Removed extra slash

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -11,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     open: true,
     port: 8080,
   },
-  base: "/portfolio/", // Base path for GitHub Pages
+  base: "/portfolio", // Base path for GitHub Pages
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
   ),
